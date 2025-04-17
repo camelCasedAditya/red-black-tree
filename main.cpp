3371d5115396d4415856ba15ed67e521153c0619
U-BSD\514957 <514957@SHS-95W2NW3.beaverton.k12.or.us>
2025-04-17 13:18:29 -0700

    rotate left function complete

--- main.cpp
+++ main.cpp
@@ -419,3 +419,27 @@ void case4(Tree* node) {
     node->setColor(BLACKCOLOR);
   }
 }
+
+void rotateLeft(Tree* pivot) {
+  if (pivot == NULL || pivot->getRight() == NULL) {
+    return;
+  }
+
+  Tree* newParent = pivot->getRight();
+  if (newParent->getLeft() == != NULL) {
+    pivot->setRight(newParent->getLeft());
+    newParent->getLeft()->setPrevious(pivot);
+  }
+  newParent->setPrevious(pivot->getPrevious());
+  if (pivot->getPrevious() == NULL) {
+    head = newParent; 
+  }
+  else if (pivot == pivot->getPrevious()->getLeft()){
+    newParent->getPrevious()->setLeft(newParent);
+  }
+  else {
+    newParent->getPrevious()->setRight(newParent);
+  }
+  newParent->setLeft(pivot);
+  pivot->setPrevious(newParent);
+}
